67a571b2b1203f833f464623438ced57ba272575
Brnd05 <grisa9547@gmail.com>
2025-08-19 12:21:41 -0600

    Actualización de interfaces

--- Interfaces/obj/Release/net9.0/Interfaces.AssemblyInfo.cs
+++ Interfaces/obj/Release/net9.0/Interfaces.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interfaces")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcc705e80b51aa01649120c35b71c243fc02613")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interfaces")]
